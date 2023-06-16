508a92341f92889c88519250eaea78bb9a326647
Isi <isi.edeoghon@gmail.com>
2023-06-16 18:32:14 +0100

    Updated myscript.js file

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -3,7 +3,7 @@ function SendMail(){
     var params = {
         from_name :document.getElementById('name').value,
         email_id :document.getElementById('email').value,
-        message :document.getElementById('message')
+        message :document.getElementById('message').value
 
 
     }
