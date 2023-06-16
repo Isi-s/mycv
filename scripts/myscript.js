f7940235e561b79486e19682c24f2e46dc3a2a63
Isi <isi.edeoghon@gmail.com>
2023-06-16 18:27:46 +0100

    Updated myscript.js file

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -8,6 +8,6 @@ function SendMail(){
 
     }
     emailjs.send('service_kv15kwn', 'template_20go6rg', params).then(function(res){
-        alert("Isi will receive your message in his email inbox!" + res.status);
+        alert("Thank you for your message. I will check my inbox and get back to you later! " + res.status);
     })
 }
